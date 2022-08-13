ab9385ab57544c9dac3dae8352d09ce78fdb9559
Yves  Kaufmann <yves@Yvess-MacBook-Pro.local>
2022-08-14 01:02:42 +0200

    added the functionality to embedd graphes based on ambigious href or anchor tags

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 import services
+import db
 
 
 app = Flask(__name__)
@@ -7,17 +8,22 @@ app = Flask(__name__)
 
 @app.route('/pairs', methods=['GET'])
 def get_href_anchor_pairs():
-    return services.get_href_anchor_pairs()
+    return services.get_href_anchor_pairs(request.args.get('url'))
 
 
 @app.route('/ambiguousHref', methods=['GET'])
 def get_href_with_ambiguous_anchor_tags():
-    return services.get_href_with_ambiguous_anchor_tags()
+    return services.get_href_with_ambiguous_anchor_tags(request.args.get('url'))
 
 
 @app.route('/ambiguousAnchorTags', methods=['GET'])
 def get_anchor_tags_with_ambiguous_href():
-    return services.get_anchor_tags_with_ambiguous_href()
+    return services.get_anchor_tags_with_ambiguous_href(request.args.get('url'))
+
+
+@app.route('/embedding', methods=['POST'])
+def graph_embedding():
+    return db.graph_embedding(services.get_anchor_tags_with_ambiguous_href(request.args.get('url')))
 
 
 if __name__ == '__main__':
--- services.py
+++ services.py
@@ -1,19 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://github.com"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
 
-
-def get_href_anchor_pairs():
+def get_href_anchor_pairs(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
     anchor_tags = []
     for link in soup.find_all("a", href=True):
         anchor_tags.append((link["href"], link.getText().replace("\n", "").replace(" ", "")))
     return anchor_tags
 
 
-def get_href_with_ambiguous_anchor_tags():
+def get_href_with_ambiguous_anchor_tags(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
     href_anchor_dict = {}
     ambiguous_anchors = {}
     for link in soup.find_all("a", href=True):
@@ -25,12 +25,15 @@ def get_href_with_ambiguous_anchor_tags():
     return ambiguous_anchors
 
 
-def get_anchor_tags_with_ambiguous_href():
+def get_anchor_tags_with_ambiguous_href(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
     href_anchor_dict = {}
     ambiguous_anchors = {}
     for link in soup.find_all("a", href=True):
-        if link["href"] != "":
-            _append_value(href_anchor_dict, link.getText().replace("\n", "").replace(" ", ""), link["href"])
+        anchor_tag = link.getText().replace("\n", "").replace(" ", "")
+        if anchor_tag != "":
+            _append_value(href_anchor_dict, anchor_tag, link["href"])
     for key in href_anchor_dict:
         if isinstance(href_anchor_dict[key], list) and len(href_anchor_dict[key]) == len(set(href_anchor_dict[key])):
             ambiguous_anchors[key] = href_anchor_dict[key]
@@ -44,5 +47,3 @@ def _append_value(dict_obj, key, value):
         dict_obj[key].append(value)
     else:
         dict_obj[key] = value
-
-
