df4d1c1fa4b92714f5e8558c5bb121ed2d8a27e9
Yves  Kaufmann <yves@Yvess-MacBook-Pro.local>
2022-08-14 12:12:14 +0200

    added new functionalities for graph embeddings, groomed code

--- app.py
+++ app.py
@@ -1,29 +1,24 @@
 from flask import Flask, request
 import services
-import db
+import repository
 
 
 app = Flask(__name__)
 
 
-@app.route('/pairs', methods=['GET'])
-def get_href_anchor_pairs():
-    return services.get_href_anchor_pairs(request.args.get('url'))
+@app.route('/combinations', methods=['GET'])
+def get_anchor_href_combinations():
+    return services.get_anchor_href_combinations(request.args.get('url'))
 
 
-@app.route('/ambiguousHref', methods=['GET'])
-def get_href_with_ambiguous_anchor_tags():
-    return services.get_href_with_ambiguous_anchor_tags(request.args.get('url'))
+@app.route('/combinations/ambiguous/hrefs', methods=['GET'])
+def get_href_with_ambiguous_anchors():
+    return services.get_href_with_ambiguous_anchors(request.args.get('url'))
 
 
-@app.route('/ambiguousAnchorTags', methods=['GET'])
-def get_anchor_tags_with_ambiguous_href():
-    return services.get_anchor_tags_with_ambiguous_href(request.args.get('url'))
-
-
-@app.route('/embedding', methods=['POST'])
-def graph_embedding():
-    return db.graph_embedding(services.get_anchor_tags_with_ambiguous_href(request.args.get('url')))
+@app.route('/combinations/ambiguous/anchors', methods=['GET'])
+def get_anchors_with_ambiguous_href():
+    return services.get_anchors_with_ambiguous_hrefs(request.args.get('url'))
 
 
 if __name__ == '__main__':
